5a6d8a5f32274397a1e47580d5523d4a2d94f04a
VioletShadow <aleszcz.szczukowska32@wp.pl>
2020-11-02 14:40:31 +0100

    Dodane Zadania

---
+++ src/auto.java
@@ -0,0 +1,34 @@
+class opony {
+  String maraka;
+  double tarcie ;
+
+}
+
+public class auto {
+ String color ;
+ int liczba_miejsc;
+ int poijemnosc_silnika;
+ double velocity;
+ opony opony;
+
+
+void  jedz(){
+    System.out.println("brum , brum");
+
+}
+
+ opony get_opony (){
+    return  opony;
+
+ }
+
+ void set_all(  String  color , int liczba_miejsc , int pojemnosc_silnika double velocity , opony opony){
+    this.color  = color;
+    this.liczba_miejsc = liczba_miejsc;
+    this.poijemnosc_silnika =pojemnosc_silnika;
+    this.velocity = velocity;
+
+ }
+
+
+     }
