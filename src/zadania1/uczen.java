ef54e6537689dbd805ce7b7538672bd54b18c0ed
VioletShadow <aleszcz.szczukowska32@wp.pl>
2020-11-05 15:39:10 +0100

    Zadania

---
+++ src/zadania1/uczen.java
@@ -0,0 +1,14 @@
+package zadania1;
+
+public class uczen {
+    public static void main(String[] args) {
+        String imie = "Ola";
+        String nazwisko = "Szczukowska";
+        int wiek = 18;
+
+        String info
+
+        }
+
+    }
+}
