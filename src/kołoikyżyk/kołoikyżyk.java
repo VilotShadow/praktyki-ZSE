611897359003ed2423d3a2c2efedec15098e0cdd
VioletShadow <aleszcz.szczukowska32@wp.pl>
2020-11-18 11:01:58 +0100

    Projekt dalszy ciąg

--- src/kołoikyżyk/kołoikyżyk.java
+++ src/kołoikyżyk/kołoikyżyk.java
@@ -1,13 +1,44 @@
 package kołoikyżyk;
 
+import java.util.Scanner;
+
 public class kołoikyżyk {
     public static void main(String[] args) {
-        int wymiar = 3;
-        char[][] plansza = //new char[wymiar][wymiar];
-                {{'O', 'X', 'X'},
-                        {'O', 'X', ' '},
-                        {' ', 'O', 'O'}};
+        System.out.println("Witaj w grze podaj rozmiary planszy ");
+    int wymiar = new Scanner(System.in).nextInt();
+    char symbolobecnegoGracza = 'X';
+    char[][] plansza = new char[wymiar][wymiar];
+    while (true) {
         kołoikyżyk.drukujPlansza(plansza);
+        boolean ruchPoprawny = wykonajRuch(plansza, symbolobecnegoGracza);
+        if (!ruchPoprawny) continue;
+        symbolobecnegoGracza = symbolobecnegoGracza == 'X' ? 'O' : 'X';
+    }
+
+
+    // if(symbolobecnegoGracza == 'X'){
+    //                symbolobecnegoGracza = 'O';
+    //            }
+    //            else {
+    //                symbolobecnegoGracza = 'X';
+    //            }
+
+}
+
+        public static  boolean wykonajRuch(char [] [] plansza, char symbol){
+        System.out.println(symbol+ " Twój ruch");
+        System.out.println(" Podaj indeks wiersza ");
+        int wiersz = new Scanner(System.in).nextInt();
+        System.out.println(" Popdaj Kolumne ");
+        int kolumna = new Scanner(System.in).nextInt();
+        boolean ruchpoprawny = plansza [wiersz] [kolumna] == 0;
+        if(!ruchpoprawny) {
+            System.out.println(" Dupa coś tam jest i zły ruch ");
+            return false ;
+        }
+            plansza [wiersz] [kolumna] = symbol;
+            return true;
+
     }
     public static void drukujPlansza (char [][] plansza ) {
         int wymiar = plansza.length;
