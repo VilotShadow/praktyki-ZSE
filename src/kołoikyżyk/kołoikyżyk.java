526ccc3910c1f8e63f4fca175100355884f85a06
VioletShadow <aleszcz.szczukowska32@wp.pl>
2020-11-20 09:32:38 +0100

    Projekt dalszy ciąg

--- src/kołoikyżyk/kołoikyżyk.java
+++ src/kołoikyżyk/kołoikyżyk.java
@@ -5,42 +5,107 @@ import java.util.Scanner;
 public class kołoikyżyk {
     public static void main(String[] args) {
         System.out.println("Witaj w grze podaj rozmiary planszy ");
-    int wymiar = new Scanner(System.in).nextInt();
-    char symbolobecnegoGracza = 'X';
-    char[][] plansza = new char[wymiar][wymiar];
-    while (true) {
-        kołoikyżyk.drukujPlansza(plansza);
-        boolean ruchPoprawny = wykonajRuch(plansza, symbolobecnegoGracza);
-        if (!ruchPoprawny) continue;
-        symbolobecnegoGracza = symbolobecnegoGracza == 'X' ? 'O' : 'X';
+        int wymiar = new Scanner(System.in).nextInt();
+        char symbolobecnegoGracza = 'X';
+        char[][] plansza = new char[wymiar][wymiar];
+
+        boolean czyKontynuować = true;
+        int licznikRuchow = 0;
+        while (czyKontynuować && licznikRuchow <= wymiar*wymiar ) {
+            kołoikyżyk.drukujPlansza(plansza);
+            boolean ruchPoprawny = wykonajRuch(plansza, symbolobecnegoGracza);
+            if (ruchPoprawny) {
+                licznikRuchow++;
+                boolean wygranaWiersze = sprawdzWiersze (plansza, symbolobecnegoGracza);
+                boolean wygranaKolumny = sprawdzKolumny (plansza, symbolobecnegoGracza);
+                boolean wygranaSkos1 = sprawdzSkos1(plansza, symbolobecnegoGracza);
+                //boolean wygranaSkos2 = sprawdzSkos2(plansza, symbolobecnegoGracza);
+                if(wygranaWiersze || wygranaKolumny || wygranaSkos1 ){
+                    System.out.println( " SUPER WYGRANA "+ symbolobecnegoGracza);
+                    czyKontynuować = false;
+                }
+                symbolobecnegoGracza = symbolobecnegoGracza == 'X' ? 'O' : 'X';
+            }
+        }
+
+
+        // if(symbolobecnegoGracza == 'X'){
+        //                symbolobecnegoGracza = 'O';
+        //            }
+        //            else {
+        //                symbolobecnegoGracza = 'X';
+        //            }
+
     }
 
+    public static boolean sprawdzWiersze(char[][] plansza, char symbol) {
+        int wymiar = plansza.length;
+        for (int wiersz = 0; wiersz < wiersz; wiersz++) {
+            boolean wygrana = true;
+            for (int kolumna = 0; kolumna < wymiar; kolumna++) {
+                if (plansza[wiersz][kolumna] != symbol) {
+                    wygrana = false;
+                    break;
+                }
+            }
+            if (wygrana){ return true;}
+        }
+        return false;
+    }
+    public static boolean sprawdzKolumny(char[][] plansza, char symbol) {
+        int wymiar = plansza.length;
+        for (int kolumna = 0; kolumna < wymiar; kolumna++) {
+            boolean wygrana = true;
+            for (int wiersz = 0; wiersz< wymiar; wiersz ++) {
+                if (plansza[wiersz][kolumna] != symbol) {
+                    wygrana = false;
+                    break;
+                }
+            }
+            if (wygrana){ return true;}
+        }
+        return false;
+    }
+    public static boolean sprawdzSkos1 (char[][] plansza, char symbol) {
+        int wymiar = plansza.length;
+        for (int i = 0; i < wymiar; i++) {
+            if (plansza[i][i]!=symbol) {
+                return false;
+            }
+        }
+        return true;
+    }
+  //  public static boolean sprawdzSkos2 (char[][] plansza, char symbol) {
+     //   int wymiar = plansza.length;
+   //     for (int i = 0; i <= wymiar; i++) {
+          //  for (int y = 2; y > wymiar; y--) {
+           //     if (plansza[i][y] != symbol) {
+            //        return false;
+        //        }
 
-    // if(symbolobecnegoGracza == 'X'){
-    //                symbolobecnegoGracza = 'O';
-    //            }
-    //            else {
-    //                symbolobecnegoGracza = 'X';
-    //            }
+      //      }
+    //    }
+       //     return true;
+    //    }
 
-}
 
-        public static  boolean wykonajRuch(char [] [] plansza, char symbol){
-        System.out.println(symbol+ " Twój ruch");
+    public static boolean wykonajRuch(char[][] plansza, char symbol) {
+        System.out.println(symbol + " Twój ruch");
         System.out.println(" Podaj indeks wiersza ");
         int wiersz = new Scanner(System.in).nextInt();
         System.out.println(" Popdaj Kolumne ");
         int kolumna = new Scanner(System.in).nextInt();
-        boolean ruchpoprawny = plansza [wiersz] [kolumna] == 0;
-        if(!ruchpoprawny) {
+        boolean ruchpoprawny = plansza[wiersz][kolumna] == 0;
+        if (!ruchpoprawny) {
             System.out.println(" Dupa coś tam jest i zły ruch ");
-            return false ;
+            return false;
         }
-            plansza [wiersz] [kolumna] = symbol;
-            return true;
+        plansza[wiersz][kolumna] = symbol;
+        return true;
 
     }
-    public static void drukujPlansza (char [][] plansza ) {
+
+    public static void drukujPlansza(char[][] plansza) {
         int wymiar = plansza.length;
 
         //nagłówki kolumn
@@ -57,8 +122,6 @@ public class kołoikyżyk {
                 System.out.print(plansza[wiersz][kolumna] + "\t");
             }
             System.out.println();
-
-
         }
     }
 }
